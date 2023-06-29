1bdbf769cf2400db8371873f8bcc8ef55afc8395
Alejandro V <70179001+alX-u@users.noreply.github.com>
2023-06-29 16:43:17 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import MyNavbar from "./components/MyNavbar";
-import "./css/index.css";
-import MyContent from "./components/MyContent";
+import MyNavbar from "./components/my_navbar";
+import "./css/my_navbar.css";
+import "./css/my_content.css";
+import MyContent from "./components/my_content";
 
 function App() {
   const [count, setCount] = useState(0);
